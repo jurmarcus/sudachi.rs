9e72e5890f1c6072e2955fca9afb45ce47f39f9c
mh-northlander <mh.northlander@gmail.com>
2025-06-02 22:16:58 +0900

    separate pos and add pos-list struct

---
+++ sudachi/src/dic/pos.rs
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2025 Works Applications Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use nom::number::complete::le_i16;
+
+use crate::error::SudachiResult;
+
+use super::read::utf16_string::short_utf16_string;
+
+pub const POS_DEPTH: usize = 6;
+
+/// A part of speech
+/// 
+/// Its length must be `POS_DEPTH`
+type POS = Vec<String>;
+
+pub struct PosList(Vec<POS>);
+
+impl Into<Vec<POS>> for PosList {
+    fn into(self) -> Vec<POS> {
+        self.0
+    }
+}
+
+impl PosList {
+    pub fn load(buf: &[u8]) -> SudachiResult<Self> {
+        let (rest, num_pos) = le_i16(buf)?;
+        let (_rest, pos_list) = nom::multi::count(
+            nom::multi::count(short_utf16_string, POS_DEPTH),
+            num_pos as usize,
+        )(rest)?;
+        Ok(PosList(pos_list))
+    }
+}
