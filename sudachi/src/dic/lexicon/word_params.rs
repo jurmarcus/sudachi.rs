c3c5c36860324f579bd2817149154ccc54bb7662
mh-northlander <mh.northlander+github@gmail.com>
2025-06-20 10:40:20 +0900

    use entry_id in word_params

--- sudachi/src/dic/lexicon/word_params.rs
+++ sudachi/src/dic/lexicon/word_params.rs
@@ -14,43 +14,43 @@
  * limitations under the License.
  */
 
+use crate::dic::word_id::EntryId;
 use crate::util::cow_array::CowArray;
-
-use super::word_infos::{word_id_to_offset};
-
 
 /// A word paratemter, used in the analysis.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct  WordParameter(u64);
+#[repr(transparent)]
+pub struct WordParameter(u64);
 
 impl WordParameter {
     /// The left connection cost id.
     #[inline]
-    pub fn left_id(self) -> i16 {
+    pub fn left_id(&self) -> i16 {
         (self.0 & 0xffff) as i16
     }
 
     /// The right connection cost id.
     #[inline]
-    pub fn right_id(self) -> i16 {
+    pub fn right_id(&self) -> i16 {
         ((self.0 >> 16) & 0xffff) as i16
     }
 
     /// The cost of the word.
     #[inline]
-    pub fn cost(self) -> i16 {
+    pub fn cost(&self) -> i16 {
         ((self.0 >> 32) & 0xffff) as i16
     }
 
     /// Return a new parameters with the given cost.
     #[inline]
-    pub fn with_cost(self, cost:i16) -> WordParameter {
+    pub fn with_cost(&self, cost: i16) -> WordParameter {
         WordParameter((cost as u64) << 32 | (self.0 & 0xffffffff))
     }
 }
 
 pub struct WordParams<'a> {
-    // word infos are aligned per 8 bytes (see WordInfos::WORD_INFO_OFFSET_ALIGNMENT).
+    // word parameters are placed at the beginning of eash word info and word infos are aligned per 8 bytes (see WordInfos::WORD_INFO_OFFSET_ALIGNMENT).
+    // Thus handling this block as u64 array is safe.
     data: CowArray<'a, u64>,
 }
 
@@ -63,23 +63,23 @@ impl<'a> WordParams<'a> {
             data: CowArray::from_bytes(bytes, 0, bytes.len()),
         }
     }
-    
+
     #[inline]
-    pub fn get_params(&self, word_id: u32) -> WordParameter {
-        let offset = word_id_to_offset(word_id);
-        // the first 8 bytes of the word info in the parameters
+    pub fn get_params(&self, entry_id: EntryId) -> WordParameter {
+        // offset in u64 array equals to the byte_offset >> 3, and byte_offset in V1 format is entry_id << 3.
+        // Thus we can skip conversion
+        let offset = entry_id.as_raw() as usize;
         WordParameter(self.data[offset])
     }
 
     #[inline]
-    pub fn get_cost(&self, word_id: u32) -> i16 {
-        let params = self.get_params(word_id);
-        params.cost()
+    pub fn get_cost(&self, entry_id: EntryId) -> i16 {
+        self.get_params(entry_id).cost()
     }
 
-    pub fn set_cost(&mut self, word_id: u32, cost: i16) {
-        let offset = word_id_to_offset(word_id);
-        let params = self.get_params(word_id);
+    pub fn set_cost(&mut self, entry_id: EntryId, cost: i16) {
+        let offset = entry_id.as_raw() as usize;
+        let params = self.get_params(entry_id);
         self.data.set(offset, params.with_cost(cost).0);
     }
 }
