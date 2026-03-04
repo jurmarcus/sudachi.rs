3448f0efc8f3c65921234d4fb5a56fa0fb2bc606
yanagisawa-yu-wap <yanagisawa-yu-wap@users.noreply.github.com>
2026-03-04 05:22:32 +0000

    deploy: 4ff4bbc3b410f88ce93a3582cd94eb168f855007

---
+++ rust/trait.impl/serde_core/de/trait.Deserialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sudachi",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"sudachi/config/enum.SurfaceProjection.html\" title=\"enum sudachi::config::SurfaceProjection\">SurfaceProjection</a>"],["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"sudachi/plugin/oov/regex_oov/enum.BoundaryMode.html\" title=\"enum sudachi::plugin::oov::regex_oov::BoundaryMode\">BoundaryMode</a>"],["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"sudachi/config/struct.ConfigBuilder.html\" title=\"struct sudachi::config::ConfigBuilder\">ConfigBuilder</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1033]}
